3be8e038642f25075611f8d13ba060d426fddc97
HassanKhan123 <hk2602809@gmail.com>
2022-01-10 14:55:30 +0500

    code refactored using inheritance

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('./football.csv');
-reader.read();
+// Refactor 1
+// import { MatchReader } from './refactor1/MatchReader';
+
+// Refactor 2
+import { CsvFileReader } from './refactor2/CsvFileReader';
+import { MatchReader } from './refactor2/MatchReader';
+
+// Refactor 1
+// const reader = new MatchReader('./football.csv');
+// reader.read();
+
+// Refactor 2
+const csvFileReader = new CsvFileReader('./football.csv');
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+// Refactor 1
+// for (let match of reader.data) {
+
+// Refactor 2
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
